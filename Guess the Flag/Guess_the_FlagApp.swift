dd472dacec48e942b83c4ee8ce4b6c33dfeea071
SeikaHirori <SeikaHirori@outlook.com>
2023-02-10 14:24:29 -0800

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Seika Hirori on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
