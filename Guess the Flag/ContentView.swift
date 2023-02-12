c400a1ad1f557dc0a5854334d458365bfa5c4d93
SeikaHirori <SeikaHirori@outlook.com>
2023-02-12 12:18:23 -0800

    Finished section 1.2 AND added a 3x3 grid using Stack and VStack

--- Guess the Flag/ContentView.swift
+++ Guess the Flag/ContentView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let defaultSpacing: CGFloat = 30.0
+    
     var body: some View {
-        HStack(spacing: 30) {
-            Text("Hello, world!")
-            Spacer()
-            Text("This is another text view :3")
-            Spacer()
-            Text("meep")
-            Spacer()
-            Text("Beep")
+        VStack(spacing: defaultSpacing) {
+            HStack(spacing: defaultSpacing) {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack(spacing: defaultSpacing) {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack(spacing: defaultSpacing) {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
